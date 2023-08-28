6727a3fc362d68b29542f35b4d4d187eb3b41bc7
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-28 11:59:00 +0800

    Create Category Entity & Related Database Operations

--- src/main/java/com/silvia/blogwebsite/Main.java
+++ src/main/java/com/silvia/blogwebsite/Main.java
@@ -3,6 +3,6 @@ package com.silvia.blogwebsite;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
     }
 }
