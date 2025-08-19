dfa3612e1e6721ea82a12d473ff645bebba6cc02
Ching-Yu Chang <sunny1218.chang@gmail.com>
2025-08-19 13:34:06 +1000

    refactor sqlConnection setting

--- src/main/java/com/silvia/blogwebsite/sqlConnector/ConnectionManager.java
+++ src/main/java/com/silvia/blogwebsite/sqlConnector/ConnectionManager.java
@@ -1,5 +1,7 @@
 package com.silvia.blogwebsite.sqlConnector;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.*;
 
 public class ConnectionManager{
@@ -10,11 +12,17 @@ public class ConnectionManager{
     // build mysql connection
     public static Connection getConnection() throws SQLException {
         if(connection == null || connection.isClosed()){
-            String database = "blog";
-            String jdbcUrl = "jdbc:mysql://localhost:3306/"+database;
-            String username = "root";
-            String password = "Sunny.1218";
-            connection = DriverManager.getConnection(jdbcUrl, username, password);
+            // read database env variables
+            Dotenv dotenv = Dotenv.load();  // 預設讀專案根目錄 .env
+            String host = dotenv.get("DB_HOST");
+            String port = dotenv.get("DB_PORT");
+            String database = dotenv.get("DB_NAME");
+            String user = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASSWORD");
+
+            //String jdbcUrl = "jdbc:mysql://localhost:3306/"+database;
+            String jdbcUrl = "jdbc:mysql://" + host + ":" + port + "/" + database;
+            connection = DriverManager.getConnection(jdbcUrl, user, password);
         }
         return connection;
     }
